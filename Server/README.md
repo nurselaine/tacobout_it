8da9752c7c89963731a76598efa346732389e68e
nurselaine <Elaine.t.huynh@gmail.com>
2022-12-08 06:00:56 +0000

    added working chat

---
+++ Server/README.md
@@ -0,0 +1,19 @@
+# taco-bout-it
+
+  Welcome to Tacobout it! A chat app created using Node js Express Socket.io (please refer to [client-side]() for more info!). Tacobout provides users the ability to join an anonymous chat rooms. 
+
+  #### Key Features
+  - Sign in page, users can pick a name and icon
+  - Users join a chat room and can send messages to other users
+  - Admins can create new chat rooms 
+  #### Stretch Goals
+  - Admins can remove users from chat rooms
+  - Utilize PostgreSQL database to store messages
+
+## Authors: Elaine Huynh
+
+## Deployed Site
+- Coming soon!
+
+## Run Locally
+- npm i express dotenv cors websocket http
--- Server/index.js
+++ Server/index.js
@@ -1,49 +1,47 @@
-const WebSocket = require('ws');
+// dependencies
+const { v4: uuidv4 } = require('uuid');
+const webSocketServer = require('websocket').server;
+const http = require('http');
 
-const server = new WebSocket.Server({
-  port: 8080
-},
-  () => {
-    console.log('Server started on port 8080');
-  }
-);
+const webSocketServerPort = 8000;
 
-const users = new Set();
+// create simple HTTP server
+const server = http.createServer();
 
-function sendMessage (message){
-  users.forEach((user) => {
-    user.ws.send(JSON.stringify(message));
-  });
-}
+// connected http server with websocket server
+server.listen(webSocketServerPort);
+console.log(`listening on PORT ${webSocketServerPort}`);
 
-server.on('connection', (ws) => {
-  console.log('joining socket connection');
-  const user = { ws };
-  users.add(user);
+// create instance of websocket server that listen on port 8000
+const wsServer = new webSocketServer({ httpServer: server});
 
-  ws.on('message', (message) => {
-    console.log(message);
-    try{
-      const data = JSON.parse(message);
-      if(typeof data.sender !== 'string' || typeof data.body !== 'string'){
-        console.error('Invalid message');
-        return;
+// Accept the handshake on recieving request from client
+const clients = {}; // hold all clients as object, user id will be added upon recieving reqeust
+
+wsServer.on('request', request => {
+  // create unqiue user ID for every user on the server
+  const userID = uuidv4();
+  console.log((new Date()) + `Recieved a new connection from origin ${request.origin}.`);
+
+  // accept incoming client request and create a new connetion
+  const connection = request.accept(null, request.origin);
+  // store connection in clients obj and use userId as the key
+  clients[userID] = connection;
+  console.log(`connection ${userID} in ${Object.getOwnPropertyNames(clients)}`);
+
+  // Now request is accepted in the server & handshake is fulfilled - status code 101 should show in browser
+
+  // Create an onmessage handler, all messages recieved by server will trigger this handler
+  connection.on('message', (message) => {
+    // all messages will be in format of utf8
+    if(message.type === 'utf8'){
+      console.log(`Recieved message: ${message.utf8Data}`);
+
+      // loop over all the clients in the clients obj and forward message that was just recieved
+      for(key in clients){
+        clients[key].sendUTF(message.utf8Data);
+        // console.log(`send message to: ${clients[key]}`);
       }
-
-      const messageToSend = {
-        sender: data.sender,
-        body: data.body,
-        sentAt: Date.now(),
-      }
-
-      sendMessage(messageToSend);
-    } catch (e) {
-      console.error('Error passing message!', e);
     }
-  });
-
-  ws.on('close', (code, reason) => {
-    users.delete(user);
-    console.log(`connection closed: ${code} ${reason}`);
   })
 })
